0b7db4addb51595f6b2aa6502dae2c1c4a656f8f
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2021-12-11 09:00:10 +0700

    Update function.js

--- function.js
+++ function.js
@@ -1,4 +1,10 @@
-window.function = function (arr) {
-  if (arr.value === undefined) return undefined;
-  return [...arr.value].reverse();
+window.function = function (text1, text2, text3) {
+  
+  text1 = text1.value ?? "";
+  text2 = text2.value ?? "";
+  text3 = text3.value ?? "";
+  
+  const arr = [];
+  arr.push(text1,text2,text3);
+  return [...arr.value];
 }
