5971da11b606fe8c891c0637fe842463ce0ab838
Thịnh Đinh <94897059+thinhdinhlca@users.noreply.github.com>
2021-12-13 16:08:12 +0700

    Update function.js

--- function.js
+++ function.js
@@ -2,7 +2,7 @@ window.function = function (input_arr,item,num_add) {
   
   if (input_arr.value === undefined) return undefined;
   item = item.value ?? "";
-  num_add = num_add.value ?? "1";
+  num_add = num_add.value ?? "0";
   
   var dup_arr = Array(num_add).fill(item);
   
